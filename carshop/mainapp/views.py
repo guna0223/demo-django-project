c70dae3eac44060cc92b9e5434b6c2c5d90e0bbb
guna0223 <gunasekar0223@gmail.com>
2026-01-06 09:20:54 +0530

    added fiel

--- carshop/mainapp/views.py
+++ carshop/mainapp/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def homeView(request):
+    template = 'mainapp/home.html'
+    contaxt = {}
+
+    return render(request, template_name= template, context= contaxt)
+
+def aboutView(request):
+    template = 'mainapp/about.html'
+    contaxt = {}
+
+    return render(request, template_name= template, context= contaxt)
+
+def contactView(request):
+    template = 'mainapp/contact.html'
+    contaxt = {}
+
+    return render(request, template_name= template, context= contaxt)
+
